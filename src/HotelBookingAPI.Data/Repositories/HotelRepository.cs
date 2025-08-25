ce0f3cabac71cebb3e70db23929edc1fbaf6a435
Tarun Mistry <TARUN732@bailliegifford.com>
2025-08-25 14:57:18 +0100

    Get all and get hotel by substring methods

--- src/HotelBookingAPI.Data/Repositories/HotelRepository.cs
+++ src/HotelBookingAPI.Data/Repositories/HotelRepository.cs
@@ -4,19 +4,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HotelBookingAPI.Data.Repositories;
 
-internal class HotelRoomRepository : IHotelRoomRepository
+internal class HotelRepository : IHotelRepository
 {
-    public HotelRoomRepository(HotelBookingDbContext dbContext)
+    public HotelRepository(HotelBookingDbContext dbContext)
     {
         _dbContext = dbContext;
     }
 
     public HotelBookingDbContext _dbContext { get; }
 
+    public async Task<IList<HotelEntity>> GetAll()
+    {
+        return await _dbContext.Hotels
+                    .AsNoTracking()
+                    .Include(hotel => hotel.Rooms)
+                    .ToListAsync();
+    }
+
     public async Task<IList<HotelEntity>> GetHotels(string hotelSubstring)
     {
         return await _dbContext.Hotels
                     .AsNoTracking()
+                    .Where(hotel => !string.IsNullOrEmpty(hotel.HotelName) 
+                        && hotel.HotelName.Contains(hotelSubstring, StringComparison.CurrentCultureIgnoreCase))
                     .Include(hotel => hotel.Rooms)
                     .ToListAsync();
     }
