5d5e8689dd764e8d874da5bfc558889aaa401468
Tarun Mistry <TARUN732@bailliegifford.com>
2025-08-25 15:31:57 +0100

    Initial logic to create Booking

---
+++ src/HotelBookingAPI.Data/Repositories/BookingRepository.cs
@@ -0,0 +1,30 @@
+﻿using HotelBookingAPI.Data.Entities;
+using HotelBookingAPI.Data.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingAPI.Data.Repositories;
+
+internal class BookingRepository : IBookingRepository
+{
+    public BookingRepository(HotelBookingDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public HotelBookingDbContext _dbContext { get; }
+
+    public async Task<int> CreateBookingEntity(BookingEntity bookingToCreate)
+    {
+        var entity = await _dbContext.AddAsync(bookingToCreate);
+        await _dbContext.SaveChangesAsync();
+        if (entity.Entity != null && entity.Entity.BookingId != null) { return (int)entity.Entity.BookingId; }
+
+        return 0;
+        // Throw Exception here
+    }
+
+    public async Task<BookingEntity?> GetBooking(int bookingId)
+    {
+        return await _dbContext.Bookings.Where(booking => booking.BookingId == bookingId).SingleOrDefaultAsync();
+    }
+}
---
+++ src/HotelBookingAPI.Data/Repositories/Interface/IBookingRepository.cs
@@ -0,0 +1,10 @@
+﻿using HotelBookingAPI.Data.Entities;
+
+namespace HotelBookingAPI.Data.Repositories.Interface;
+
+public interface IBookingRepository
+{
+    Task<BookingEntity?> GetBooking(int bookingId);
+
+    Task<int> CreateBookingEntity(BookingEntity bookingToCreate);
+}
