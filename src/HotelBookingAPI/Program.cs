a5eea941d71e4c416acb7c15dcf6b4abf0ecf9dc
Tarun Mistry <TARUN732@bailliegifford.com>
2025-08-25 11:55:46 +0100

    Register database options from appsettings

--- src/HotelBookingAPI/Program.cs
+++ src/HotelBookingAPI/Program.cs
@@ -3,12 +3,16 @@ using HotelBookingAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDataLayerAccess();
+
+builder.Services.AddOptions<DatabaseServiceOptions>()
+            .Bind(builder.Configuration.GetRequiredSection("Database"))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
 
 var app = builder.Build();
 
