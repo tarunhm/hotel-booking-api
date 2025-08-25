591076f30eafa6d1c7edc548794b6b52b17cb35b
Tarun Mistry <TARUN732@bailliegifford.com>
2025-08-25 10:58:07 +0100

    Create database service options in data project

---
+++ src/HotelBookingAPI.Data/DatabaseServiceOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelBookingAPI.Data;
+
+internal class DatabaseServiceOptions
+{
+    public string? Username { get; set; }
+
+    public string? Password { get; init; }
+
+    public string? ServiceName { get; set; } = "g3cb1be755ffe93_tmdev01_high.adb.oraclecloud.com";
+
+    public string? Host { get; set; } = "adb.uk-london-1.oraclecloud.com";
+
+    public string ConnectionString => $";User Id={Username};Password={Password};Data Source=(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host={Host}))(connect_data=(service_name={ServiceName}))(security=(ssl_server_dn_match=yes)));";
+
+    public bool AreServiceOptionsValid =>
+        !string.IsNullOrEmpty(Username) &&
+        !string.IsNullOrEmpty(Password);
+}
