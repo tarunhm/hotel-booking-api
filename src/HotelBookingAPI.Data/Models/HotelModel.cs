ffacadfc70cbcff0a89f07cf2890e7834b603b22
Tarun Mistry <TARUN732@bailliegifford.com>
2025-08-25 15:25:48 +0100

    Hotel controllerto get all and get hotel by search terms

---
+++ src/HotelBookingAPI.Data/Models/HotelModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingAPI.Data.Models;
+
+public class HotelModel
+{
+    public int Id { get; set; }
+
+    public string? HotelName { get; set; }
+
+    public ICollection<RoomModel> Rooms { get; set; } = [];
+}
---
+++ src/HotelBookingAPI/Controllers/HotelController.cs
@@ -0,0 +1,51 @@
+﻿using HotelBookingAPI.Data.Models;
+using HotelBookingAPI.Data.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.Controllers;
+
+[Route("api/[controller]/[action]")]
+public class HotelController: ControllerBase
+{
+    private IHotelService _hotelService { get; set; }
+
+    public HotelController(IHotelService hotelService)
+    {
+        _hotelService = hotelService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IList<HotelModel>>> Get([Required] string searchTerm)
+    {
+        if (!ModelState.IsValid) {  return BadRequest(ModelState); }
+
+        try
+        {
+            var hotels = await _hotelService.GetBySubstring(searchTerm);
+            if (hotels.Any()) { return Ok(hotels); }
+
+            return NotFound();
+        }
+        catch
+        {
+            return StatusCode(500, "An Internal Server Error Occured");
+        }
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IList<HotelModel>>> GetAll()
+    {
+        try
+        {
+            var hotels = await _hotelService.GetAll();
+            if (hotels.Any()) { return Ok(hotels); }
+
+            return NotFound();
+        }
+        catch
+        {
+            return StatusCode(500, "An Internal Server Error Occured");
+        }
+    }
+}
