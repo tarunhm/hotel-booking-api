db78c6bf22dce9cfad3744a2bad497d9d4200d6a
Tarun Mistry <TARUN732@bailliegifford.com>
2025-08-25 11:02:14 +0100

    Add service collection extension for data project

---
+++ src/HotelBookingAPI.Data/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using HotelBookingAPI.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelBookingAPI.Data;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddDataLayerAccess(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddDbContext<HotelBookingDbContext>();
+        serviceCollection.AddScoped<IHotelRoomRepository, HotelRoomRepository>();
+        return serviceCollection;
+    }
+}
