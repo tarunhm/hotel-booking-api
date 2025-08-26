b4eea85e1289b08a0b7d6e04cd94b164bfb854ea
Tarun Mistry <TARUN732@bailliegifford.com>
2025-08-26 10:23:36 +0100

    Available rooms controller with get method

---
+++ src/HotelBookingAPI/Controllers/AvailableRoomsController.cs
@@ -0,0 +1,39 @@
+﻿using HotelBookingAPI.Data.Services.Interface;
+using HotelBookingAPI.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.Controllers;
+
+[Route("api/[controller]/[action]")]
+public class AvailableRoomsController : ControllerBase
+{
+    private IBookingService _bookingService { get; }
+
+    public AvailableRoomsController(IBookingService bookingService)
+    {
+        _bookingService = bookingService;
+    }
+
+    [HttpGet]
+    public ActionResult<RoomModel> Get([Required] AvailibilityRequestModel availibilityRequest)
+    {
+        if (!ModelState.IsValid) 
+        {
+            return BadRequest(ModelState); 
+        }
+
+        try
+        {
+            var booking = _bookingService.GetAvailableRooms(availibilityRequest.People, availibilityRequest.CheckInDate, availibilityRequest.Nights);
+
+            if (!booking.Any()) { return NotFound(); }
+
+            return Ok(booking);
+        }
+        catch (Exception ex) 
+        { 
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
