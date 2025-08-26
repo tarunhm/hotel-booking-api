fa3db7b83c2de27af5dc121bcfcc83b939af4c02
Tarun Mistry <TARUN732@bailliegifford.com>
2025-08-26 10:19:27 +0100

    Register new services and repository for data layer

--- src/HotelBookingAPI.Data/ServiceCollectionExtension.cs
+++ src/HotelBookingAPI.Data/ServiceCollectionExtension.cs
@@ -1,4 +1,7 @@
 ﻿using HotelBookingAPI.Data.Repositories;
+using HotelBookingAPI.Data.Repositories.Interface;
+using HotelBookingAPI.Data.Services;
+using HotelBookingAPI.Data.Services.Interface;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HotelBookingAPI.Data;
@@ -8,7 +11,13 @@ public static class ServiceCollectionExtension
     public static IServiceCollection AddDataLayerAccess(this IServiceCollection serviceCollection)
     {
         serviceCollection.AddDbContext<HotelBookingDbContext>();
-        serviceCollection.AddScoped<IHotelRoomRepository, HotelRoomRepository>();
+        serviceCollection.AddScoped<IHotelRepository, HotelRepository>();
+        serviceCollection.AddScoped<IRoomRepository, RoomRepository>();
+        serviceCollection.AddScoped<IBookingRepository, BookingRepository>();
+
+        serviceCollection.AddScoped<IBookingService, BookingService>();
+        serviceCollection.AddScoped<IHotelService, HotelService>();
+
         return serviceCollection;
     }
 }
