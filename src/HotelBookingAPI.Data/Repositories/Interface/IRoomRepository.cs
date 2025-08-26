1d0527b3620d1b5934131d2b7b8d1a00d3e77c10
Tarun Mistry <TARUN732@bailliegifford.com>
2025-08-26 10:18:24 +0100

    Room repository to get by capacity and check room exists by ID

---
+++ src/HotelBookingAPI.Data/Repositories/Interface/IRoomRepository.cs
@@ -0,0 +1,10 @@
+﻿using HotelBookingAPI.Data.Entities;
+
+namespace HotelBookingAPI.Data.Repositories.Interface;
+
+public interface IRoomRepository
+{
+    IEnumerable<RoomEntity> GetByCapacity(int people);
+
+    Task<bool> ExistsById(int roomId);
+}
---
+++ src/HotelBookingAPI.Data/Repositories/RoomRepository.cs
@@ -0,0 +1,30 @@
+﻿using HotelBookingAPI.Data.Entities;
+using HotelBookingAPI.Data.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingAPI.Data.Repositories;
+
+internal class RoomRepository : IRoomRepository
+{
+    public RoomRepository(HotelBookingDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public HotelBookingDbContext _dbContext { get; }
+
+    public IEnumerable<RoomEntity> GetByCapacity(int people)
+    {
+        return _dbContext.Rooms
+            .Where(room => room.RoomCapacity >= people)
+            .Include(room => room.Bookings)
+            .Include(room => room.Hotel)
+            .AsNoTracking()
+            .AsEnumerable();
+    }
+
+    public async Task<bool> ExistsById(int roomId)
+    {
+        return await _dbContext.Rooms.AnyAsync(room => room.Id.Equals(roomId));
+    }
+}
