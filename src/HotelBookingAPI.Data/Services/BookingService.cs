feb5fcf87f3306ed06aff35efab9813285de236d
Tarun Mistry <TARUN732@bailliegifford.com>
2025-08-26 10:20:21 +0100

    Initial logic for booking data service

---
+++ src/HotelBookingAPI.Data/Services/BookingService.cs
@@ -0,0 +1,108 @@
+﻿using HotelBookingAPI.Data.Entities;
+using HotelBookingAPI.Data.Repositories.Interface;
+using HotelBookingAPI.Data.Services.Interface;
+using HotelBookingAPI.Shared.Models;
+
+namespace HotelBookingAPI.Data.Services;
+
+public class BookingService : IBookingService
+{
+    public BookingService(IBookingRepository bookingRepository, IRoomRepository roomRepository)
+    {
+        _bookingRepository = bookingRepository;
+        _roomRepository = roomRepository;
+    }
+
+    private IBookingRepository _bookingRepository { get; }
+    public IRoomRepository _roomRepository { get; }
+
+    public async Task<BookingModel?> GetById(int id)
+    {
+        var booking = await _bookingRepository.GetBooking(id);
+        if (booking == null) { return null; }
+
+        //TODO = extract mapping
+        return new BookingModel() 
+        { 
+            BookingId = id, 
+            BookingUser = booking.BookingUser, 
+            CheckInDate = DateOnly.FromDateTime(booking.CheckInDate), 
+            CheckOutDate = DateOnly.FromDateTime(booking.CheckOutDate),
+            RoomId = booking.RoomId,
+            RoomType = booking.Room.RoomType,
+            RoomCapacity = booking.Room.RoomCapacity,
+            HotelId = booking.Room.HotelId,
+            HotelName = booking.Room.Hotel?.HotelName!
+        };
+    }
+
+    public async Task<int> CreateBooking(BookingRequestModel bookingRequest)
+    {
+        // Check room exists
+        if (!await _roomRepository.ExistsById(bookingRequest.RoomId))
+        {
+            throw new InvalidOperationException($"Room with ID {bookingRequest.RoomId} does not exist.");
+        }
+
+        //Check availability here
+        //ToDo: Extract mapping
+        var bookingEntity = new BookingEntity()
+        {
+            RoomId = bookingRequest.RoomId,
+            CheckInDate = bookingRequest.CheckInDate.ToDateTime(new TimeOnly()),
+            CheckOutDate = bookingRequest.CheckInDate.AddDays(bookingRequest.Nights).ToDateTime(new TimeOnly()),
+            BookingUser = bookingRequest.BookingUser
+        };
+
+        return await _bookingRepository.CreateBookingEntity(bookingEntity);
+    }
+
+    public IList<RoomModel> GetAvailableRooms(int people, DateOnly checkInDate, int duration)
+    {
+        var rooms = _roomRepository.GetByCapacity(people);
+        if (!rooms.Any()) { return []; }
+
+        var availableRooms = rooms.Where(room => IsRoomAvailable(room.Bookings, checkInDate, duration));
+
+        return availableRooms.Select(room => new RoomModel() { RoomId = room.Id, HotelId = room.HotelId, HotelName = room.Hotel?.HotelName!, RoomType = room.RoomType, RoomCapacity = room.RoomCapacity}).ToList();
+    }
+
+    private static bool IsRoomAvailable(ICollection<BookingEntity> bookings, DateOnly checkInDate, int newBookingDuration)
+    {
+        if (!bookings.Any()) { return true; }
+        var overlapBookings = bookings.Where(b => BookingOverlaps(DateOnly.FromDateTime(b.CheckInDate), 
+                                             DateOnly.FromDateTime(b.CheckOutDate),
+                                             checkInDate,
+                                             newBookingDuration));
+
+        return !overlapBookings.Any();
+    }
+
+    private static bool BookingOverlaps(DateOnly checkInDate, DateOnly checkOutDate, DateOnly newBookingCheckInDate, int newBookingDuration)
+    {
+        int daysBetweenCheckIn = newBookingCheckInDate.DayNumber - checkInDate.DayNumber;
+
+        // Check in of booking is after proposed booking
+        if (daysBetweenCheckIn < 0) 
+        {
+            // if proposed stay is shorter than duration then no overlap
+            // if duration is longer than days between check in, then overlap
+            // If duration is the same as days between check in then no overlap
+            return newBookingDuration > Math.Abs(daysBetweenCheckIn);
+        }
+
+        // Check in of booking is before the proposed booking
+        if (daysBetweenCheckIn > 0) 
+        {
+            int bookingDuration = checkOutDate.DayNumber - checkInDate.DayNumber;
+
+            // if existing duration is shorter than difference then no overlap
+            // if existing duration is longer than days between check in, then overlap
+            // If existing duration is the same as days between check in then no overlap
+            return bookingDuration > daysBetweenCheckIn;
+        }
+        
+        // If booking starts on same date
+        return true;
+    }
+}
---
+++ src/HotelBookingAPI.Data/Services/Interface/IBookingService.cs
@@ -0,0 +1,12 @@
+﻿using HotelBookingAPI.Shared.Models;
+
+namespace HotelBookingAPI.Data.Services.Interface;
+
+public interface IBookingService
+{
+    Task<int> CreateBooking(BookingRequestModel bookingRequest);
+
+    Task<BookingModel?> GetById(int id);
+
+    IList<RoomModel> GetAvailableRooms(int people, DateOnly checkInDate, int duration);
+}
