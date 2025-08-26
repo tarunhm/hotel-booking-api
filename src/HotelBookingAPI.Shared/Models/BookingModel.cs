1aff814d50c2ed785ae6aeb7a3b5aa9d1cc246bb
Tarun Mistry <TARUN732@bailliegifford.com>
2025-08-26 10:20:41 +0100

    Room and booking model return types

---
+++ src/HotelBookingAPI.Shared/Models/BookingModel.cs
@@ -0,0 +1,22 @@
+﻿namespace HotelBookingAPI.Shared.Models;
+
+public class BookingModel
+{
+    public int BookingId { get; set; }
+
+    public string? BookingUser { get; set; } = null!;
+
+    public DateOnly CheckInDate { get; set; } 
+
+    public DateOnly CheckOutDate { get; set; }
+
+    public int RoomId { get; set; }
+
+    public string? RoomType { get; set; } = null!;
+
+    public int RoomCapacity { get; set; }
+
+    public int HotelId { get; set; }
+
+    public string? HotelName { get; set; } = null!;
+}
---
+++ src/HotelBookingAPI.Shared/Models/RoomModel.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelBookingAPI.Shared.Models;
+
+public class RoomModel
+{
+    public int RoomId { get; set; }
+
+    public int HotelId { get; set; }
+
+    public string HotelName { get; set; } = null!;
+
+    public string RoomType { get; set; } = null!;
+
+    public int RoomCapacity { get; set; }
+}
